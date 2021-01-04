e928005cd4c0b9f62ecbe37571f2d22e865613c3
diksha-139 <64410938+diksha-139@users.noreply.github.com>
2021-01-04 17:55:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,50 +1,71 @@
-var bullet,wall;
-var speed, weight;
-var thickness;
-var damage=0;
+var wall, thickness;
+var bullet,speed, weight;
 
 function setup() {
-  createCanvas(1500,400);
+  createCanvas(1600, 400);
 
-  bullet = createSprite(50, 200, 100, 30);
-  bullet.shapeColor="white";
- 
- 
-  wall = createSprite(1200,200,thickness,height/2);
-  wall.shapeColor=color(80,80,80);
+  speed=random(223,321)
+  weight=random(30,52)
+
+
+  thickness=random(22,83)
+
+
+
 
   
-  speed = random(223,321);
-  weight = random(30,52);
-  thickness=random(23,83);
+    bullet=createSprite(50, 200, 50,5);  
+    bullet.velocityX = speed;
+    bullet.shapeColor=color(255);
 
-  damage=(0.5*weight*speed*speed)/(thickness*thickness*thickness);
+ 
+  	
+    
+    wall=createSprite(1200, 200, thickness, height/2);  
 
-
+    wall.shapeColor=color(230,230,230);
+  //wall.shapeColor=color(80,80,80)
 }
+
 
 function draw() {
-  background("cyan");  
-  
-  
+  background(0);  
+  //bullet.sprite.collide(wall.sprite,calculateDeformation)
+  if(hasCollided(bullet, wall))
+  {
+  	bullet.velocityX=0;
+  	var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
 
-  if(keyDown("space")){
-    bullet.velocityX=speed;
+  	
+	if(damage>10)
+	{
+		wall.shapeColor=color(255,0,0);
+		
+	}
 
+	
+
+	if(damage<10)
+	{
+		wall.shapeColor=color(0,255,0);
+	}
+	
   }
-if(bullet.isTouching(wall) && damage<10){
-  bullet.shapeColor=color(0,255,0);
-  bullet.velocityX=0;
-  textSize(30);
-  text("Damage is :"+damage,200,100);
-}
-if(bullet.isTouching(wall) && damage>10){
-  bullet.shapeColor="red";
-  bullet.velocityX=0;
-  textSize(30);
-  text("Damage is :"+damage,200,100);
+
+
+  drawSprites();
+ 
 }
 
 
-drawSprites();
+function hasCollided(lbullet, lwall)
+{
+	bulletRightEdge=lbullet.x +lbullet.width;
+	wallLeftEdge=lwall.x;
+	if (bulletRightEdge>=wallLeftEdge)
+	{
+		return true
+	}else
+  {
+	return false;
 }
